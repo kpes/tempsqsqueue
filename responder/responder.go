83b7aa378f52ceba4a64046f22900c8a9efe6cb1
Keith Pesavento <5579587+kpes@users.noreply.github.com>
2022-10-16 14:54:33 -0400

    adding responder, untested

--- responder/responder.go
+++ responder/responder.go
@@ -2,21 +2,43 @@ package responder
 
 import (
 	"context"
+	"tempqueue/common"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
 )
 
+type ProcessFunc func(message string) (string, error)
+
 type Responder struct {
-	client *sqs.Client
+	client      *sqs.Client
+	processFunc ProcessFunc
 }
 
-func NewResponder(sqsClient *sqs.Client) Responder {
+func NewResponder(sqsClient *sqs.Client, processFunc ProcessFunc) Responder {
 	return Responder{
-		client: sqsClient,
+		client:      sqsClient,
+		processFunc: processFunc,
 	}
 }
 
-func (r *Responder) ProcessAndReply(ctx context.Context, message string) error {
+func (r *Responder) ProcessAndReply(ctx context.Context, message types.Message) error {
+	responseUrl := message.MessageAttributes[common.ResponseQueueUrl]
+	correlationId := message.MessageAttributes[common.CorrelationMessageAttributeKey]
 
-	return nil
+	result, err := r.processFunc(*message.Body)
+	if err != nil {
+		return err
+	}
+
+	_, err = r.client.SendMessage(ctx, &sqs.SendMessageInput{
+		QueueUrl:    responseUrl.StringValue,
+		MessageBody: aws.String(result),
+		MessageAttributes: map[string]types.MessageAttributeValue{
+			common.CorrelationMessageAttributeKey: correlationId,
+		},
+	})
+
+	return err
 }
